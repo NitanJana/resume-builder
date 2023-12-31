60404d3df4765289cc88a862a2f7c77eaf6830f4
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-12-31 12:12:43 +0530

    feat: add personal details state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,76 @@ import ContentTab from './ContentTab';
 
 import '../styles/App.css';
 
+import { useState } from 'react';
+
 function App() {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    const phoneRegex = /^[0-9\s]*$/;
+    if (name === 'phoneNumber' && !phoneRegex.test(value)) {
+      return; // Do not update state for invalid phone numbers
+    }
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="w-full h-screen bg-gray-100 grid grid-cols-[1fr_4fr_5fr] font-poppins">
       <div className=""></div>
 
-      <section className="p-8 flex flex-col gap-8">
+      <section className="p-8 flex flex-col gap-8 ">
         <div className="content-tab ">
           <h2 className="text-2xl font-bold mb-5">Personal Details</h2>
           <div className="flex flex-col gap-3">
-            <InputField label="Full Name" type="text" name="fullName" id="fullName" autoComplete="name" />
+            <InputField
+              label="Full Name"
+              type="text"
+              name="fullName"
+              id="fullName"
+              autoComplete="name"
+              value={formData.fullName}
+              onChange={handleChange}
+            />
 
-            <InputField label="Email" type="email" name="email" id="email" autoComplete="email" />
+            <InputField
+              label="Email"
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
 
-            <InputField label="Phone Number" type="tel" name="phNumber" id="phNumber" autoComplete="tel" />
+            <InputField
+              label="Phone Number"
+              type="tel"
+              name="phoneNumber"
+              id="phoneNumber"
+              autoComplete="tel"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+            />
 
-            <InputField label="Address" type="text" name="address" id="address" autoComplete="street-address" />
+            <InputField
+              label="Address"
+              type="text"
+              name="address"
+              id="address"
+              autoComplete="street-address"
+              value={formData.address}
+              onChange={handleChange}
+            />
           </div>
         </div>
 
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
-const InputField = ({ label, type, name, id, autoComplete }) => {
+const InputField = ({ label, type, name, id, autoComplete, value,onChange }) => {
   return (
     <div>
       <label htmlFor={id} className="label-primary">
         {label}
       </label>
-      <input autoComplete={autoComplete} type={type} name={name} id={id} className="input-primary" />
+      <input
+        autoComplete={autoComplete}
+        type={type}
+        name={name}
+        id={id}
+        value={value}
+        onChange={onChange}
+        className="input-primary"
+      />
     </div>
   );
 };
@@ -16,6 +24,8 @@ InputField.propTypes = {
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   autoComplete: PropTypes.string.isRequired,
+  value: PropTypes.any.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default InputField;
